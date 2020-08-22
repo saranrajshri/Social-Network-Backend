dc1452a10e8f22fdd1adeeb2180810afd4efe34e
saranrajshri <shrisaranraj2001@gmail.com>
2020-08-23 01:03:55 +0530

    Adds Secuity packages

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const hpp = require("hpp");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
 const routes = require("./routes/");
 const {
   errorHandler,
@@ -11,8 +16,19 @@ const {
 
 require("./intializers/DBInitializer");
 
+// Rate Limiter
+const limiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
 app.use(cors());
 app.use(bodyParser.json());
+app.use(limiter);
+app.use(xss()); // santize body, params, url
+app.use(hpp()); // To prevent HTTP parameter pollution attack
+app.use(helmet()); // To secure from setting various HTTP headers
+app.use(mongoSanitize());
 
 // log only 4xx and 5xx responses to console
 app.use(
